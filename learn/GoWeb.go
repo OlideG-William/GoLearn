9cafc760a22ab739d276294a14f7e6b4c7c3fa6a
Itan <breethbloods8@gmail.com>
2023-09-11 17:33:52 +0300

    Add file and folder to github

---
+++ learn/GoWeb.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Test() {
+	fmt.Println("hello is a 2 comit in file")
+	fmt.Println(43 + 232)
+}
+
+/*
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+	http.ListenAndServe(":80", nil)
+}
+*/
---
+++ les6/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	maessage := make(chan string)
+
+	go func() {
+
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 300)
+			maessage <- fmt.Sprintf("%d", i)
+		}
+
+		close(maessage)
+	}()
+
+	for v := range maessage {
+
+		fmt.Println(v)
+	}
+
+}
