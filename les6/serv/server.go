751cc2167f4a2a61b125ea3a3f542e80349a4f9a
Itan <breethbloods8@gmail.com>
2023-09-19 12:58:09 +0300

    change and delete file

---
+++ les6/serv/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		time.Sleep(300 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
