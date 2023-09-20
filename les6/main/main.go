713d7c39e1b452afd40f525bc2fea73faab1d1b1
Itan <breethbloods8@gmail.com>
2023-09-21 00:10:24 +0300

    Delete folter and change main file

--- les6/main/main.go
+++ les6/main/main.go
@@ -2,53 +2,53 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
+type WaitGroup struct {
+	counter int64
+}
+
+func (w *WaitGroup) Add(n int64) {
+
+	atomic.AddInt64(&w.counter, n)
+}
+
+func (w *WaitGroup) Done() {
+
+	w.Add(-1)
+	if atomic.LoadInt64(&w.counter) < 0 {
+		panic("negative wait group counter")
+	}
+}
+
+func (w *WaitGroup) Wait() {
+
+	for {
+
+		if atomic.LoadInt64(&w.counter) == 0 {
+			return
+		}
+	}
+}
+
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
-	go task1(&wg)
-	go task2(&wg)
-	go task3(&wg)
-	go task4(&wg)
-	go task5(&wg)
+
+	var wg WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		time.Sleep(300 * time.Millisecond)
+		fmt.Println("go routine 1")
+
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("go routine 2")
+	}()
+
 	wg.Wait()
-}
-
-func task1(wg *sync.WaitGroup) {
-	defer wg.Done()
-	_, err := http.Get("http://localhost:4554")
-	if err != nil {
-		log.Fatalf("could not make http request: %v", err)
-	}
-	fmt.Println("task1 : done")
-}
-
-func task2(wg *sync.WaitGroup) {
-	defer wg.Done()
-	var count int
-	for i := 0; i < 1_000_000_000; i++ {
-		count += i
-	}
-	fmt.Println("task 2: done")
-}
-
-func task3(wg *sync.WaitGroup) {
-	fmt.Println("task3: done")
-	defer wg.Done()
-}
-
-func task4(wg *sync.WaitGroup) {
-	time.Sleep(500 * time.Millisecond)
-	fmt.Println("task4: done")
-	defer wg.Done()
-}
-
-func task5(wg *sync.WaitGroup) {
-	fmt.Println("task5: done")
-	wg.Done()
+	fmt.Println("all go routines done!")
 }
