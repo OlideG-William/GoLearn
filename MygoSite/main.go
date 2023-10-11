765ab16a768554ac98d9577ec3d8c12b3c9547b9
Itan <breethbloods8@gmail.com>
2023-10-11 13:28:38 +0300

    Small web sites

--- MygoSite/main.go
+++ MygoSite/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home")
+	templ.ExecuteTemplate(w, "main", &Page{Title: "Welcome to TL;DR"})
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("About")
+	templ.ExecuteTemplate(w, "about", &Page{Title: "About TL;DR"})
+}
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", homepageHandler)
+	http.HandleFunc("/about", aboutHandler)
+	fmt.Println("Server started on port 8080")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
 
-	// Define a handler for your web page
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("MygoSite/templates/index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+var templ = func() *template.Template {
+	t := template.New("")
+	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".html") {
+			fmt.Println(path)
+			_, err = t.ParseFiles(path)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
-		tmpl.Execute(w, nil)
+		return err
 	})
 
-	// Start the web server
-	http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}()
+
+type Page struct {
+	Title string
 }
