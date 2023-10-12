e271bb4a5c02a8cd177b7aae77c4f6d36488b584
Itan <breethbloods8@gmail.com>
2023-10-12 18:23:12 +0300

    Decoder use for .json files

--- les6/main.go
+++ les6/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
 
+type Student struct {
+	Name     string
+	Standard int `json:"Standard"`
+}
+
+func main() {
+	StudentFile, err := os.Open("data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer StudentFile.Close()
+
+	var studentDecoder *json.Decoder = json.NewDecoder(StudentFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var studentList []Student
+
+	err = studentDecoder.Decode(&studentList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, student := range studentList {
+		fmt.Println("Student", i+1)
+		fmt.Println("Student name", student.Name)
+		fmt.Println("Student standart: ", student.Standard)
+		fmt.Printf("\n")
+	}
 }
