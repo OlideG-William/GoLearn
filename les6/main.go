d8adcb42321aa18228b581c3833f1b8926991290
Itan <breethbloods8@gmail.com>
2023-10-26 12:06:57 +0300

    direct file path learn

--- les6/main.go
+++ les6/main.go
@@ -25,14 +25,12 @@ func CreateFile() {
 }
 
 func ReadFile() {
-
 	data, err := os.ReadFile("newTextfile.txt")
 	check(err)
 	fmt.Println(string(data))
 }
 
 func WriteFile() {
-	
 	data, err := os.OpenFile("newTextfile.txt", os.O_WRONLY|os.O_APPEND, 0644)
 	check(err)
 	defer data.Close()
@@ -46,10 +44,9 @@ func CopyFile() {
 	// Open another file and we grant file rights
 	copt, err := os.OpenFile("copyinffile.txt", os.O_WRONLY, 0666)
 	check(err)
-	
 	_, err = io.Copy(copt, newdat)
 	check(err)
-	
+
 	defer newdat.Close()
 	defer copt.Close()
 
@@ -58,18 +55,18 @@ func CopyFile() {
 func RenameFile() {
 	err := os.Chmod("copyinffile.txt", 0777)
 	check(err)
-	err = os.Rename("copyinffile.txt","newrenamefile.txt")
+	err = os.Rename("copyinffile.txt", "newrenamefile.txt")
 	check(err)
 }
 
-func DeletedFile(){
+func DeletedFile() {
 	err := os.Chmod("newrenamefile.txt", 0777)
 	check(err)
 	err = os.Remove("newrenamefile.txt")
 	check(err)
 }
 
-func CheckAvailabilityFile(){
+func CheckAvailabilityFile() {
 
 }
 
@@ -83,6 +80,43 @@ func main() {
 	//ReadFile()
 }
 
+// Function for converting lowercase letters to uppercase and vice versa
+// and remove all symbol:^%$#@#$.... Except: '!'
+/*
+func LineFilter() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		runes := []rune(scanner.Text())
+
+		for i, r := range runes {
+			if unicode.IsLetter(r) {
+
+				if unicode.IsUpper(r) {
+					runes[i] = unicode.ToLower(r)
+				} else if unicode.IsLower(r) {
+					runes[i] = unicode.ToUpper(r)
+				}
+			} else if runes[i] == '!' {
+				continue
+			} else {
+				runes[i] = ' '
+			}
+		}
+
+		str := string(runes)
+		CleanString := strings.Join(strings.Fields(str), " ")
+		fmt.Println(CleanString)
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
+	}
+}
+*/
+
+//OpenRead file to string
 /*
 func OpenFile() {
 
@@ -98,18 +132,5 @@ func OpenFile() {
 		panic(err)
 	}
 	fmt.Println(string(str[6]))
-}
-
-func Readfile() {
-	dataR, err := os.ReadFile("testino.txt")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dataR))
-}
-
-func main() {
-	OpenFile()
-	//Readfile()
 }
 */
