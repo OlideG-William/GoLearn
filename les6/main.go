174a8a369bbe796cc2d1286c3ea91b27d28bc24c
Itan <breethbloods8@gmail.com>
2023-10-29 23:25:13 +0200

    Task done for file read/write... in main.go file

--- les6/main.go
+++ les6/main.go
@@ -1,15 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"unicode"
 )
 
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Function for converting lowercase letters to uppercase and vice versa
+// and remove all symbol:^%$#@#$.... Except: '!'
+func LineFilter() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		runes := []rune(scanner.Text())
+
+		for i, r := range runes {
+			if unicode.IsLetter(r) {
+
+				if unicode.IsUpper(r) {
+					runes[i] = unicode.ToLower(r)
+				} else if unicode.IsLower(r) {
+					runes[i] = unicode.ToUpper(r)
+				}
+			} else if runes[i] == '!' {
+				continue
+			} else {
+				runes[i] = ' '
+			}
+		}
+
+		str := string(runes)
+		CleanString := strings.Join(strings.Fields(str), " ")
+		fmt.Println(CleanString)
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
+	}
+}
+
+// OpenRead file to string
+func OpenFile() {
+
+	f, err := os.Open("testino.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	str := make([]byte, 2048)
+	_, err = f.Read(str)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(str[6]))
 }
 
 func CreateFile() {
@@ -67,70 +121,92 @@ func DeletedFile() {
 }
 
 func CheckAvailabilityFile() {
-
+	filename := "data.json"
+	directory := "../les6"
+	_, err := os.Open(directory + "/" + filename)
+	if os.IsNotExist(err) {
+		fmt.Printf("The file %s does not exist %s\n", filename, directory)
+	} else {
+		fmt.Printf("The file %s exists %s\n", filename, directory)
+	}
 }
+
+func ReadLinebyLineFile() {
+	data, err := os.Open("newTextfile.txt")
+	check(err)
+	defer data.Close()
+
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+}
+
+func SearchTextInFile() {
+
+	file, err := os.Open("newTextfile.txt")
+	check(err)
+	defer file.Close()
+
+	someText := "you"
+	scanner := bufio.NewScanner(file)
+	var count int
+	found := false
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), someText) {
+			found = true
+			count++
+		}
+	}
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+
+	if found {
+		fmt.Println("In file:", file.Name(), "Was found text: `",
+			someText, "`", count)
+	} else {
+		fmt.Println("Text `", someText, "` not in file")
+	}
+}
+
+func BinaryFile() {
+
+	dataBinar := []byte("Data to binary file\nfsdfsfsdf \nfixi dixi\n")
+	err := os.WriteFile("Batfile", dataBinar, 0644)
+	check(err)
+
+	fopen, err := os.Open("Batfile")
+	if err != nil {
+		panic(err)
+	}
+	defer fopen.Close()
+
+	reader := bufio.NewScanner(fopen)
+	for reader.Scan() {
+		b := reader.Text()
+		fmt.Println(b)
+	}
+}
+
+//Advanced task for file
 
 func main() {
 
+	//BinaryFile()
+	//SearchTextInFile()
+	//ReadLinebyLineFile()
+	//CheckAvailabilityFile()
 	//DeletedFile()
 	//RenameFile()
 	//CopyFile()
 	//CreateFile()
 	//WriteFile()
 	//ReadFile()
+	//OpenFile()
+	//LineFilter()
 }
-
-// Function for converting lowercase letters to uppercase and vice versa
-// and remove all symbol:^%$#@#$.... Except: '!'
-/*
-func LineFilter() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		runes := []rune(scanner.Text())
-
-		for i, r := range runes {
-			if unicode.IsLetter(r) {
-
-				if unicode.IsUpper(r) {
-					runes[i] = unicode.ToLower(r)
-				} else if unicode.IsLower(r) {
-					runes[i] = unicode.ToUpper(r)
-				}
-			} else if runes[i] == '!' {
-				continue
-			} else {
-				runes[i] = ' '
-			}
-		}
-
-		str := string(runes)
-		CleanString := strings.Join(strings.Fields(str), " ")
-		fmt.Println(CleanString)
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "error:", err)
-			os.Exit(1)
-		}
-	}
-}
-*/
-
-//OpenRead file to string
-/*
-func OpenFile() {
-
-	f, err := os.Open("testino.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	str := make([]byte, 2048)
-	_, err = f.Read(str)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(str[6]))
-}
-*/
--- les6/test/filepath.go
+++ les6/test/filepath.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
+	"sort"
+	"time"
 )
 
-func main() {
-	p := filepath.Join("dir1", "dir2", "filename")
-	fmt.Println("p:", p)
-
-	fmt.Println(filepath.Join("dir//", "filename"))
+func compile() {
+	a := []int{5, 3, 4, 7, 8, 9}
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] < a[j]
+	})
+	for _, v := range a {
+		fmt.Println(v)
+	}
 }
+
+
+func main() {
+
+	start_non_gor := time.Now()
+	compile()
+	endtime_non_gor := time.Since(start_non_gor)
+	fmt.Println("time compile with gorutines: ", endtime_non_gor)
+
+	start := time.Now()
+	go compile()
+	time.Sleep(time.Nanosecond)
+	endtime := time.Since(start)
+	fmt.Println("time compile gorutines: ", endtime)
+
+}
+
